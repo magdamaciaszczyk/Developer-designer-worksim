bbfc0b594522ede3a87e6283085c5bf9f607a142
Magda Faizov <magda@dxw.com>
2017-04-07 14:09:02 +0100

    Changes to index.php, added js and partials folders

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
+<html>
 
 <head>
     <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/main.css" type="text/css">
 </head>
 
 <body>
-
-    <header>
-    </header>
-
-    <main>
-    </main>
-
-    <footer>
-    </footer>
 
 </body>
 
